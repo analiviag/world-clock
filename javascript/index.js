d5d9bf14fbc1221d30bcfe99f61e42e8faa0bd19
analiviag <126291217+analiviag@users.noreply.github.com>
2024-05-07 16:06:17 -0300

    injected Javascript in the code to make date and time work

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,30 @@
+function displayDateAndTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+
+  newYorkDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+
+  parisDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  parisTimeElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+displayDateAndTime();
+setInterval(displayDateAndTime, "1000");
